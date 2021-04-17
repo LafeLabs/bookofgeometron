5c9d7c4d29c148dd305fb9c9c7ec08aa860a3f69
Lafe Spietz <lafelabs@gmail.com>
2021-04-17 11:29:09 -0400

    random tiny changes

---
+++ makescrolls.sh
@@ -0,0 +1,16 @@
+echo "foo"
+pandoc civilizations.tex -o backscrolls/civilizations.md
+pandoc organicmedia.tex -o backscrolls/organicmedia.md
+pandoc streetnetwork.tex -o backscrolls/streetnetwork.md
+pandoc code.tex -o backscrolls/code.md
+pandoc scrolls.tex -o backscrolls/scrolls.md
+pandoc maps.tex -o backscrolls/maps.md
+pandoc feeds.tex -o backscrolls/feeds.md
+pandoc symbols.tex -o backscrolls/symbols.md
+pandoc web2d.tex -o backscrolls/web2d.md
+pandoc shapes.tex -o backscrolls/shapes.md
+pandoc machines.tex -o backscrolls/machines.md
+pandoc geometron3d.tex -o backscrolls/geometron3d.md
+pandoc ontology.tex -o backscrolls/ontology.md
+pandoc trashrobot.tex -o backscrolls/trashrobot.md
+pandoc fullstack.tex -o backscrolls/fullstack.md
